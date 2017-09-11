2fff1b836c29d4a9c5528d94e4959c375dbb48c1
Marsdin Davidson <mdavidson10@matrix.senecac.on.ca>
2017-09-11 16:26:05 -0400

    Add existing files

---
+++ WS01/at-home/w1_home.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+void main() {
+	printf("******************************\n");
+	printf("** Welcome to C Programming ** \n");
+        printf("******************************\n");
+
+	return 0;
+}
---
+++ WS01/in-lab/workshop1.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+void main() {
+	printf(">**Welcome to c programming**< \n");
+	return 0;
+}
